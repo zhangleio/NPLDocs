dbf8e7f057447523a77c9f71f254fdb675bcf991
Zhiyuan <zyguochn@gmail.com>
2017-03-11 22:54:19 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ sys.path.append('../breathe')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.pngmath', 'sphinx.ext.todo', 'breathe']
+extensions = ['sphinx.ext.githubpages', 'sphinx.ext.pngmath', 'sphinx.ext.todo', 'breathe']
 breathe_projects = { "nutshell": "../xml"}
 breathe_default_project = "nutshell"
 # Add any paths that contain templates here, relative to this directory.
@@ -91,7 +91,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
